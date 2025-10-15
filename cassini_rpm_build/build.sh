0ad2b6e7c383ea961cc67cc79a0aa3c0a9497f6e
rakesh-kumar.boyanapalli@hpe.com <rakesh-kumar.boyanapalli@hpe.com>
2025-10-15 15:10:37 +0530

    Update build.sh to use appropriate ce-devops branch (#503)

--- cassini_rpm_build/build.sh
+++ cassini_rpm_build/build.sh
@@ -6,9 +6,18 @@
 set -Eeuox pipefail
 
 CE_BUILD_SCRIPT_REPO=hpc-shs-ce-devops
-CE_CONFIG_BRANCH=${CE_CONFIG_BRANCH:-main}
+
+# Use appropriate ce-devops branch
+if [[ -n "${BRANCH_NAME:-}" && "$BRANCH_NAME" =~ "release/shs-" ]]; then
+    CE_CONFIG_BRANCH=$BRANCH_NAME
+elif [[ -n "${CHANGE_TARGET:-}" && "$CHANGE_TARGET" =~ "release/shs-" ]]; then
+    CE_CONFIG_BRANCH=$CHANGE_TARGET
+else
+    CE_CONFIG_BRANCH=${CE_CONFIG_BRANCH:-main}
+fi
 
 if [ -d ${CE_BUILD_SCRIPT_REPO} ]; then
+    git -C ${CE_BUILD_SCRIPT_REPO} fetch
     git -C ${CE_BUILD_SCRIPT_REPO} checkout ${CE_CONFIG_BRANCH}
     git -C ${CE_BUILD_SCRIPT_REPO} pull
 else
